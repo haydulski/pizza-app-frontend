b4a391c78f886ed5d7de90cd6e229418a94590d9
haydulski <damian.hajdukiewicz@gmail.com>
2022-08-02 23:09:09 +0200

    fix heroku

--- src/components/UserUpdateModal.js
+++ src/components/UserUpdateModal.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import { displayMessage } from '@/lib/displayMessage'
+import { displayMessage } from '../lib/displayMessage'
 
 const UserUpdate = ({ isUpdate, hide }) => {
 
