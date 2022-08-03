d59149def7e1831ea9aeeb972f298105252fbfb6
haydulski <damian.hajdukiewicz@gmail.com>
2022-08-03 11:04:06 +0200

    fix heroku

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -25,7 +25,6 @@ export default async function handler(req, res) {
         })
         .catch(err => {
             if (err.response) {
-                console.log(err.response.data.errors);
                 res.status(206).json(err.response.data.errors)
             }
         })
